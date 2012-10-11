524460731a38af4765048f79ba4999f7514dffc9
Efe Gürkan YALAMAN <efeyalaman@gmail.com>
2012-10-12 01:00:29 +0300

    Otel'e yolcu yerleştirme ilk implementasyonu
    Otel'e yolcu eklemek için yapılan ilk implementasyon. Buna uygun olacak şekilde hazır yazılmış kodlarda düzenlemeler yapıldı.
    Artan olması durumu için ilk eklemeler yapıldı. Oluşturulmuş dizilerin return ile diğer metodlara aktarılması sağlandı.

--- Main.cs
+++ Main.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,14 +12,16 @@ namespace proje1
     class Otel
     {
 
-        public string otel_isimleri;
-        public int kontenjan;
+        public string otel_isim;
+        public int kontenjan,kalan;
+		public double yuzde;
+		public ArrayList kalanlar = new ArrayList();
 
-        public Otel() { }
+        public Otel() { kalan=0; yuzde=0; }
         public void bilgi_al()
         {
             Console.WriteLine("Otel adını giriniz:");
-            otel_isimleri = Console.ReadLine();
+            otel_isim = Console.ReadLine();
             Console.WriteLine("Kontenjanı giriniz:");
             kontenjan = Convert.ToInt32(Console.ReadLine());
         }
@@ -47,7 +50,7 @@ namespace proje1
         public Islem(int konuk_sayısı, string[,] matris_isim, string[] dizi_dil)
         { konuk_say = konuk_sayısı; isim_matrisi = matris_isim; dil_dizisi = dizi_dil; }
 
-        public void nesne_dizi_olusturma()
+        public Konuk[] konuk_dizi_olusturma()
         {
             Konuk[] konuklar = new Konuk[konuk_say];
             Random random_sayi1 = new Random((int)DateTime.Now.Ticks);
@@ -67,6 +70,7 @@ namespace proje1
                 konuklar[i].soyad = isim_matrisi[1, b];
                 konuklar[i].dil = dil_dizisi[c];
             }
+			return konuklar;
         }
     }
 
@@ -82,23 +86,63 @@ namespace proje1
 			string[,] ad_soyad={{"ibrahim","vladimir","emre","johnny","mona","sherlock","bruce","michael","padme","mace","christopher","frodo","omer","obiwan","anakin","john","morgan","david","lee","christian"},
                 {"uzum","dost","lenin","vitamin","jin","oztekin","cakir","depp","yong","skywalker","kenobi","wayne","windu","baggins","amidala","corleone","holmes","connor","lisa","freeman"}};
             string[] yabanci_dil = { "TR", "ENG", "GER", "FRE", "JAP", "CHN", "RUS" };
-
-			otel_bilgisi_alma();
-			Islem bir_islem=new Islem(konuk_sayisi_alma(),ad_soyad,yabanci_dil);
 			
-		}
-		static void otel_bilgisi_alma()
-		{
+			
 			Console.WriteLine("Otel sayisini giriniz:");
-			int sayi=Convert.ToInt32(Console.ReadLine());
+			int otelsayi=Convert.ToInt32(Console.ReadLine());
+			
+			Otel[] otelList = otel_bilgisi_alma(otelsayi);
+			
+			int konuk_say = konuk_sayisi_alma();
+			Islem bir_islem=new Islem(konuk_say,ad_soyad,yabanci_dil);
+			
+			otel_yerleştirme(otelList,bir_islem.konuk_dizi_olusturma(),konuk_say,otelsayi);
+		}
+		static void otel_yerleştirme(Otel[] bir_otel,Konuk[] konuklist, int konuksayisi, int otelSayi)
+		{
+			int toplamKontenjan = 0,dolasimSayaci=0;
+			double bolunecekOran, yerlesecek;
+			
+			for(int i=0; i < otelSayi;++i)
+			{
+				toplamKontenjan += bir_otel[i].kontenjan;
+			}
+			bolunecekOran = (double)toplamKontenjan/konuksayisi;
+			
+			for(int i = 0 ; i < otelSayi; ++i)
+			{
+				yerlesecek=Math.Round((double)bir_otel[i].kontenjan/bolunecekOran);
+				for(int j =dolasimSayaci; j < yerlesecek; ++j)
+				{
+					bir_otel[i].kalanlar.Add(konuklist[j]);
+					++bir_otel[i].kalan;
+					++dolasimSayaci;
+					--konuksayisi;
+				}
+				bir_otel[i].yuzde = (double)bir_otel[i].kalan/(double)bir_otel[i].kontenjan;
+			}
+			/*if(konuksayisi>0)
+			{
+				Queue<int> artanekleme = new Queue<int>();
+				for(int i=0; i<otelSayi;++i)
+				{
+					
+				}
+			}
+			*/
+		}
+		
+		static Otel[] otel_bilgisi_alma(int sayi)
+		{
+			
 			
 			Otel[] bir_otel=new Otel[sayi];
 			for(int i=0; i<sayi; i++)
 			{   
 				bir_otel[i]=new Otel();
 				bir_otel[i].bilgi_al();
-				
 			}
+			return bir_otel;
 		}
 		static int konuk_sayisi_alma()
 		{
