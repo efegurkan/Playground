ad8f994f28c7b8b8e7a4a0d24ae2687ce62a7ee6
Efe Gürkan YALAMAN <efeyalaman@gmail.com>
2012-10-14 00:04:01 +0300

    anadilİndex,TransferListesi,TransferEt İlk hali
    Verilen dilin indexini döndüren metot yazıldı. dilKontrol buna göre
    düzenlendi.Transfer edilmesi gerekenleri ArrayList halinde aktaran metot
    yazıldı. Transfer etme metodu yazıldı. Bu metot otellerde boşluk olması
    halinde birini uygun olarak aktarıyor. Yorum satırı olarak eklenen yer
    replace işlemi için bir ilk hal. Yeniden tasarlanması gerekiyor.

--- DSprojeKonferans/Program.cs
+++ DSprojeKonferans/Program.cs
@@ -16,8 +16,9 @@ namespace proje1
         public int kontenjan, kalan;
         public double yuzde;
         public ArrayList kalanlar = new ArrayList();
+        public int[] kontrol = {0,0,0,0,0,0,0};
 
-        public Otel() { kalan = 0; yuzde = 0; }
+        public Otel() { kalan = 0; yuzde = 0;}
         public void bilgi_al()
         {
             Console.WriteLine("Otel adını giriniz:");
@@ -31,6 +32,7 @@ namespace proje1
     class Konuk
     {
         public string ad, soyad, dil;
+        public int kaldigiIndex;
         public Konuk() { }
         public void yazdır()
         {
@@ -115,6 +117,8 @@ namespace proje1
                 for (int j = dolasimSayaci; j < yerlesecek; ++j)
                 {
                     bir_otel[i].kalanlar.Add(konuklist[j]);
+                    konuklist[j].kaldigiIndex=anadilIndex(konuklist[j].dil);
+                    ++bir_otel[i].kontrol[anadilIndex(konuklist[j].dil)];
                     ++bir_otel[i].kalan;
                     ++dolasimSayaci;
                     --konuksayisi;
@@ -145,7 +149,59 @@ namespace proje1
             }
         }
 
-        static void dilKontrol(Konuk[] konukList)
+        static int anadilIndex(string dil)
+        {
+            int donus=-1;
+            switch (dil)
+            {
+                case "TR":
+                    donus=0;
+                    break;
+                case "ENG":
+                    donus = 1;
+                    break;
+                case "GER":
+                    donus = 2;
+                    break;
+                case "FRE":
+                    donus = 3;
+                    break;
+                case "JAP":
+                    donus = 4;
+                    break;
+                case "CHN":
+                    donus = 5;
+                    break;
+                case "RUS":
+                    donus = 6;
+                    break;
+            }
+            return donus;
+        }
+        static ArrayList transferListesiAl(Otel[] otelList,ArrayList tek)
+        {
+            Konuk konuk = new Konuk();
+            ArrayList transferList = new ArrayList();
+            for (int i = 0; i < otelList.Length; i++)//otelleri dolaş
+            {
+                foreach (int j in otelList[i].kontrol)//otelde anadil kontrol listesini dolaş
+                {
+                    if (otelList[i].kontrol[j] == 1)//yalnız kalan var mı?
+                    {
+                        for (int k = 0; k < otelList[i].kalanlar.Count; k++)//varsa otelde kalanları dolaş
+                        {
+                            konuk = (Konuk)otelList[i].kalanlar[j];
+                            if(j==anadilIndex(konuk.dil) && tek.Contains(konuk)==false)//j ile belirlenen yalnız kalan dil tutuyorsa ve yalnız kalmak zorunda değilse
+                            {
+                                transferList.Add(otelList[i].kalanlar[j]);//transfer listesine ekle
+                            }
+                        }
+                    }
+                }
+            }
+            return transferList;//transfer listesini döndür
+        }
+        static ArrayList dilKontrol(Konuk[] konukList)
         {
             int[] kontrol = {0,0,0,0,0,0,0};
             string[] arama = { "TR", "ENG", "GER", "FRE", "JAP", "CHN", "RUS" };
@@ -154,30 +210,8 @@ namespace proje1
 
             for(int i=0; i<konukList.Length; i++)//Konuk tek kalma için listesi kontrol edilir
             {
-                
-                switch(konukList[i].dil){
-                    case "TR":
-                        kontrol[0]++;
-                        break;
-                    case "ENG":
-                        kontrol[1]++;
-                        break;
-                    case "GER":
-                        kontrol[2]++;
-                        break;
-                    case "FRE":
-                        kontrol[3]++;
-                        break;
-                    case "JAP":
-                        kontrol[4]++;
-                        break;
-                    case "CHN":
-                        kontrol[5]++;
-                        break;
-                    case "RUS":
-                        kontrol[6]++;
-                        break;
-            }
+
+                ++kontrol[anadilIndex(konukList[i].dil)];
                 foreach (int j in kontrol)
                 {
                     if ( kontrol[j] == 1 )
@@ -201,10 +235,90 @@ namespace proje1
                 Console.WriteLine("Devam etmek için bir tuşa basın.");
                 Console.ReadKey();
             }
-            //return tek;
+            return tek;
 
         }
+        static void transferEt(Otel[] otelList,Konuk[] konukList ,ArrayList tek)
+        {
+            ArrayList transfer = new ArrayList();
+            Konuk konukNesne = new Konuk();
+            Konuk konukKarsilastirma = new Konuk();
+            int toplamKont=0;
+            transfer = transferListesiAl(otelList, tek);
 
+            for (int i = 0; i < otelList.Length; i++)
+                toplamKont += otelList[i].kontenjan;
+
+            if (konukList.Length != toplamKont)
+            {
+                foreach (int i in transfer)
+                {
+                    konukNesne = (Konuk)transfer[i];
+                    for (int j = 0; j + 1 < transfer.Count; j++)
+                    {
+                        konukKarsilastirma = (Konuk)transfer[j + 1];
+                        if (konukKarsilastirma.Equals(konukNesne) == true && otelList[konukNesne.kaldigiIndex].kontenjan - otelList[konukNesne.kaldigiIndex].kalan != 0)
+                        {
+                            otelList[konukNesne.kaldigiIndex].kalanlar.Add(konukKarsilastirma);
+                            ++otelList[konukNesne.kaldigiIndex].kontrol[anadilIndex(konukKarsilastirma.dil)];
+                            otelList[konukKarsilastirma.kaldigiIndex].kalanlar.Remove(konukKarsilastirma);
+                            --otelList[konukKarsilastirma.kaldigiIndex].kontrol[anadilIndex(konukKarsilastirma.dil)];
+                            konukKarsilastirma.kaldigiIndex = konukNesne.kaldigiIndex;
+                            transfer.Remove(konukNesne);
+                            transfer.Remove(konukKarsilastirma);
+                            break;
+                        }
+                        else if (otelList[konukKarsilastirma.kaldigiIndex].kontenjan - otelList[konukKarsilastirma.kaldigiIndex].kalan != 0)
+                        {
+                            otelList[konukKarsilastirma.kaldigiIndex].kalanlar.Add(konukNesne);
+                            ++otelList[konukKarsilastirma.kaldigiIndex].kontrol[anadilIndex(konukNesne.dil)];
+                            otelList[konukNesne.kaldigiIndex].kalanlar.Remove(konukNesne);
+                            --otelList[konukNesne.kaldigiIndex].kontrol[anadilIndex(konukNesne.dil)];
+                            konukNesne.kaldigiIndex = konukKarsilastirma.kaldigiIndex;
+                            transfer.Remove(konukNesne);
+                            transfer.Remove(konukKarsilastirma);
+                            break;
+                        }
+                    }
+                }
+            }
+            //else
+            //{
+            //    int transferKendiIki = 0,transferBaskaIndex=0;
+            //    foreach (int i in transfer)
+            //    {
+            //        konukNesne = (Konuk)transfer[i];
+            //        for (int j = 0; j < otelList[konukNesne.kaldigiIndex].kontrol.Length; j++)
+            //            if (otelList[konukNesne.kaldigiIndex].kontrol[j] != 0)
+            //            {
+            //                transferKendiIki =j;
+            //                break;
+            //            }
+            //        for (int j = 0; j < otelList.Length; j++)
+            //        {
+            //            bool flag = false;
+            //            for (int k = 0; k < otelList[j].kontrol.Length; k++)
+            //            {
+
+            //                if (otelList[j].kontrol[k] > 2 && konukNesne.kaldigiIndex != j)
+            //                {
+            //                    transferBaskaIndex = k;
+            //                    flag = true;
+            //                    break;
+            //                }
+
+            //            }
+            //            if (flag)
+            //                break;
+            //        }
+            //        for(int j=0; j< otelList[transferBaskaIndex].kontrol.Length;j++)
+            //        {
+
+            //        }
+            //    }
+            //}
+
+        }
         static Otel[] otel_bilgisi_alma(int sayi)
         {
 
